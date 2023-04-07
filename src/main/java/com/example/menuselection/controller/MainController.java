8231c8351daf68fdf099c602490fe7136179ebb9
ParkJongWoo <jwp0124@naonsoft.com>
2023-04-07 13:09:45 +0900

    테이블 생성

--- src/main/java/com/example/menuselection/controller/MainController.java
+++ src/main/java/com/example/menuselection/controller/MainController.java
@@ -18,10 +18,20 @@ import java.util.stream.Collectors;
 public class MainController {
     private final MyMenuRepository repository;
     @GetMapping("/test")
-    public String test(MenuDTO dto, Model model){
-        List<MenuDTO> menuList = repository.findAll(Sort.by(Sort.Direction.ASC,"MENU_ID")).stream().map(Menu::toDTO).collect(Collectors.toList());
-        model.addAttribute("list", menuList);
+    public String test(){
+
         return "test";
+    }
+
+    @GetMapping("/main")
+    public String select(MenuDTO dto, Model model){
+
+        List<MenuDTO> menuList = repository.findAll(Sort.by(Sort.Direction.ASC,"menuId")).stream().map(Menu::toDTO).collect(Collectors.toList());
+        System.out.println("=================================");
+        System.out.println(menuList);
+        System.out.println("=================================");
+        model.addAttribute("list", menuList);
+        return "select";
     }
 
 }
