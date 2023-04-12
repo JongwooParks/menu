a2234dc63768759bc4a6dc9a58d3a61fd19f9c76
ParkJongWoo <jwp0124@naonsoft.com>
2023-04-12 13:20:53 +0900

    restController 기반작업, service 생성

--- src/main/java/com/example/menuselection/controller/MenuRestController.java
+++ src/main/java/com/example/menuselection/controller/MenuRestController.java
@@ -1,10 +1,46 @@
 package com.example.menuselection.controller;
 
+import com.example.menuselection.domain.Menu;
+import com.example.menuselection.domain.MenuDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/menu")
 public class MenuRestController {
+
+
+    @PutMapping("/validate")
+    public ResponseEntity<?> validateMenu(MenuDTO data, BindingResult result){
+        if(result.hasErrors()){
+           return checkError(result);
+        }
+        return new ResponseEntity<MenuDTO>(data,HttpStatus.OK);
+    }
+    @PostMapping("/register")
+    public ResponseEntity<?> insert(MenuDTO data, BindingResult result){
+        if(result.hasErrors()){
+            return checkError(result);
+        }
+        return new ResponseEntity<MenuDTO>(data,HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> checkError(BindingResult result){
+        Map<String, String> errorMap = new HashMap<>();
+        for(FieldError error : result.getFieldErrors()){
+            errorMap.put(error.getField(),error.getDefaultMessage());
+        }
+        return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+    }
+
 
 }
---
+++ src/main/java/com/example/menuselection/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.example.menuselection.service;
+
+import com.example.menuselection.domain.MenuDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface MenuService {
+    public List<MenuDTO> selectAll(MenuDTO dto);
+
+    public int insertMenu(MenuDTO dto);
+}
